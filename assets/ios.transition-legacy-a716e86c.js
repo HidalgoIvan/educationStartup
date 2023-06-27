818adbf80f6954191d026bda2f32482ce0e3a5d8
Ernesto Iván Ochoa Hidalgo <hidalgoivanhi@gmail.com>
2023-06-27 14:29:55 +0200

    Updates

--- assets/ios.transition-legacy-a716e86c.js
+++ assets/ios.transition-legacy-a716e86c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3400edf4.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.a,n=e.g}],execute:function(){
+System.register(["./index-legacy-03d73b8a.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.a,n=e.g}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
